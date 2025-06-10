bd705f8bef753dcd9e305d4d81cb4b0bdaf701cb
charlie <charlieyeh1114@gmail.com>
2025-06-10 23:59:45 +0800

    by adding command to debug

--- acs-frontend/docker-entrypoint.sh
+++ acs-frontend/docker-entrypoint.sh
@@ -6,9 +6,12 @@ echo "Injecting runtime environment variables..."
 if [ ! -f /usr/share/nginx/html/assets/env.template.js ]; then
   echo "❌ Missing env.template.js! Did you forget to COPY the right file?" >&2
   exit 1
+else
+  echo "✅ Found env.template.js, proceeding..."
 fi
 
 : "${API_URL:=http://localhost:8080}"
+
 
 sed "s|__API_URL__|${API_URL}|g" /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
 
