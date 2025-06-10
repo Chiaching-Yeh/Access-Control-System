67f691f822946ecf03ad724301176b67865fa0f7
charlie <charlieyeh1114@gmail.com>
2025-06-10 23:30:40 +0800

    by adding command to debug

--- acs-frontend/docker-entrypoint.sh
+++ acs-frontend/docker-entrypoint.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+set -e  # 碰到錯誤就終止 script
+set -u  # 使用未定義的變數會報錯
+
 echo "Injecting runtime environment variables..."
 
-: "${API_URL:=http://localhost:8080}"  # 預設值（可 override）
+if [ ! -f /usr/share/nginx/html/assets/env.template.js ]; then
+  echo "❌ Missing env.template.js! Did you forget to COPY the right file?" >&2
+  exit 1
+fi
 
-cat /usr/share/nginx/html/assets/env.template.js | \
-  sed "s|__API_URL__|${API_URL}|g" > /usr/share/nginx/html/assets/env.js
+: "${API_URL:=http://localhost:8080}"
+
+sed "s|__API_URL__|${API_URL}|g" /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
 
 echo "Starting Nginx..."
 
