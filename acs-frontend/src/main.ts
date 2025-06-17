0eb34aa445fda00d41108ed39637494e2e26b05e
charlie <charlieyeh1114@gmail.com>
2025-06-18 02:38:41 +0800

    fix component error

--- acs-frontend/src/main.ts
+++ acs-frontend/src/main.ts
@@ -1,11 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http'; 
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    provideHttpClient() ,
     // 其他 providers 可以在這裡添加
   ]
 }).catch(err => console.error(err));
