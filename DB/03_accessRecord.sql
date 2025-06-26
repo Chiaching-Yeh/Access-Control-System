ed07186326c699634000dc26c6706d4ddf41c240
charlie <charlieyeh1114@gmail.com>
2025-06-26 14:30:47 +0800

    debug path

--- DB/03_accessRecord.sql
+++ DB/03_accessRecord.sql
@@ -1,10 +1,10 @@
 CREATE TABLE accessRecord (
-	recordUid UUID PRIMARY KEY,
-	cardId varchar(50) NOT NULL,
-	accessTime timestamp NOT NULL,
-	reason text NULL,
-	deviceId varchar(50) NOT NULL,
-	successful bool NOT NULL,
+    recordUid UUID PRIMARY KEY,
+    cardId varchar(50) NOT NULL,
+    accessTime timestamp NOT NULL,
+    reason text NULL,
+    deviceId varchar(50) NOT NULL,
+    successful bool NOT NULL
 );
 
 CREATE INDEX idx_cardId ON accessRecord(cardId);
