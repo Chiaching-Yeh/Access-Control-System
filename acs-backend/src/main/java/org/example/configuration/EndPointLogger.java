4530dc368279d75e9b5b09124880eeae5e7f382d
charlie <charlieyeh1114@gmail.com>
2025-06-26 12:51:16 +0800

    add debug logger

---
+++ acs-backend/src/main/java/org/example/configuration/EndPointLogger.java
@@ -0,0 +1,22 @@
+package org.example.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+
+@Component
+public class EndPointLogger implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Autowired
+    private RequestMappingHandlerMapping mapping;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        mapping.getHandlerMethods().forEach((key, value) ->
+                System.out.println("📍 註冊的 endpoint: " + key));
+    }
+
+}
