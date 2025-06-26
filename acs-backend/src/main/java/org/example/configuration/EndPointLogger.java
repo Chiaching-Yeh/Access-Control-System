bd20a23d04352a00d77d37a98600a2e540190eaa
charlie <charlieyeh1114@gmail.com>
2025-06-26 13:17:54 +0800

    debug with @Qualifier to use specific bean

--- acs-backend/src/main/java/org/example/configuration/EndPointLogger.java
+++ acs-backend/src/main/java/org/example/configuration/EndPointLogger.java
@@ -1,6 +1,7 @@
 package org.example.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 public class EndPointLogger implements ApplicationListener<ApplicationReadyEvent> {
 
     @Autowired
+    @Qualifier("requestMappingHandlerMapping")
     private RequestMappingHandlerMapping mapping;
 
     @Override
