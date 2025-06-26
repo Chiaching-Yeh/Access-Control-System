e8b81ec1e21665e105f8e567a2229af511393e2a
charlie <charlieyeh1114@gmail.com>
2025-06-26 18:08:15 +0800

    debug

--- acs-common/src/main/java/org/example/dao/UserInterface.java
+++ acs-common/src/main/java/org/example/dao/UserInterface.java
@@ -19,8 +19,8 @@ public interface UserInterface extends SqlObject {
     @SqlQuery("SELECT * FROM USERS WHERE userId = ? ")
     Optional<User> findByUserId(String userId);
 
-    @SqlUpdate("INSERT INTO users (userId, name, cardId, isActive, qrCode, updatedAt, createdAt) " +
-            "VALUES (:userId, :name, :cardId, :isActive, :qrCode, :updatedAt , :createdAt)")
+    @SqlUpdate("INSERT INTO users (userId, name, cardId, isActive, updatedAt, createdAt) " +
+            "VALUES (:userId, :name, :cardId, :isActive, :updatedAt , :createdAt)")
     int insert(@BindBean User user);
 
 }
