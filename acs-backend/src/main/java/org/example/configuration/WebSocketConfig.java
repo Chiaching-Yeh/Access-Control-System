522c90d7d9afac4a533f16e8cea9540558e66426
charlie <charlieyeh1114@gmail.com>
2025-06-27 12:14:39 +0800

    add dependency to make sure support STOMP over WebSocket

--- acs-backend/src/main/java/org/example/configuration/WebSocketConfig.java
+++ acs-backend/src/main/java/org/example/configuration/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package org.example.configuration;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -8,13 +11,16 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.lang.reflect.Method;
+import java.util.Set;
+
 @Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends BeanConfiguration implements WebSocketMessageBrokerConfigurer {
 
     /**
-     * 控制哪些網域能成功「握手」建立 WebSocket 連線
+     * 控制哪些網域能成功「握手」建立 WebSocket 連線、註冊 STOMP WebSocket 端點，例如 /ws/access
      * @param registry
      */
     @Override
@@ -23,6 +29,9 @@ public class WebSocketConfig extends BeanConfiguration implements WebSocketMessa
         registry.addEndpoint("/ws/access").setAllowedOriginPatterns(allowedOriginsPath).withSockJS();
     }
 
+    /**
+     * 設定 STOMP 訊息代理的路由前綴
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
