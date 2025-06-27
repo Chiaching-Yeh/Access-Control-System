ae23408a27bba422a62e600b5b57ff31819849d1
charlie <charlieyeh1114@gmail.com>
2025-06-27 13:53:12 +0800

    debug to make sure support STOMP over WebSocket

--- acs-backend/src/main/java/org/example/configuration/WebSocketConfig.java
+++ acs-backend/src/main/java/org/example/configuration/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package org.example.configuration;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.servlet.ServletContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,26 +36,5 @@ public class WebSocketConfig extends BeanConfiguration implements WebSocketMessa
         registry.setApplicationDestinationPrefixes("/app");
     }
 
-    @PostConstruct
-    public void checkWebSocketSupport() {
-        System.out.println("=== WebSocket Support Check ===");
-        try {
-            // 1. 檢查類別是否存在
-            Class.forName("jakarta.websocket.server.ServerContainer");
-            Class.forName("org.apache.tomcat.websocket.server.WsServerContainer");
-            System.out.println("All WebSocket classes loaded successfully");
-
-            // 2. 檢查 ServletContext 中是否有 ServerContainer (這才是關鍵!)
-            Object serverContainer = servletContext.getAttribute("jakarta.websocket.server.ServerContainer");
-            if (serverContainer != null) {
-                System.out.println(" ServerContainer properly initialized: " + serverContainer.getClass().getName());
-            } else {
-                System.out.println("ServerContainer NOT initialized in ServletContext");
-            }
-
-        } catch (Exception e) {
-            System.err.println("WebSocket class loading failed: " + e.getMessage());
-        }
-    }
 
 }
