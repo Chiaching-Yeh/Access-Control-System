8c57fb01ca48f88749e654da8293d1a6bee8954d
charlie <charlieyeh1114@gmail.com>
2025-06-29 14:02:02 +0800

    add qrCode success / failure component

---
+++ acs-frontend/src/app/component/QrFailure/qrFailure.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qrFailure',
+  imports: [],
+  templateUrl: './qrFailure.component.html',
+  styleUrl: './qrFailure.component.css'
+})
+export class QrFailureComponent {
+
+}
