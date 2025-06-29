1be2be12a1a67c51831925709c706b675c18265a
charlie <charlieyeh1114@gmail.com>
2025-06-29 14:01:29 +0800

    add qrCode success / failure component

--- acs-frontend/src/app/app.module.ts
+++ acs-frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 // import { BrowserModule } from '@angular/platform-browser';
 // import { NgModule} from '@angular/core';
 // import { AppRoutingModule } from './app.routes';
-// import { FormsModule } from '@angular/forms'; 
+// import { FormsModule } from '@angular/forms';
 // import { AppComponent } from './app.component';
-// import { QrPageComponent } from './compoment/QrPage/qrPage.component';
+// import { QrPageComponent } from './component/QrPage/qrPage.component';
 
 // @NgModule({
 //   declarations: [
@@ -15,7 +15,7 @@
 //     AppRoutingModule,  // 路由模組，如果沒加這一行 AppRoutingModule，使用者輸入網址時，不知道要去哪
 //     FormsModule
 //   ],
-//   providers: [], 
+//   providers: [],
 //   bootstrap: [AppComponent]
 // })
-// export class AppModule { } 
+// export class AppModule { }
